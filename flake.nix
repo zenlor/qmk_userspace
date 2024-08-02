4ec0288892b955e9e245e1040335e863a745f578
Lorenzo Giuliani <lorenzo@quantfi.tech>
2024-08-02 17:12:04 +0200

    use flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "qmk userspace";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
+    flakelight.url = "github:nix-community/flakelight";
+  };
+  outputs = {self, flakelight, nixpkgs, ...}: flakelight ./. {
+    inherit inputs;
+    systems = nixpkgs.lib.systems.flakeExposed;
+
+    devShell.packages = pkgs: with pkgs; [
+      qmk
+      nixpkgs-fmt
+      nixd
+    ];
+  };
+}
