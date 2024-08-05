091dd2ece9822caac6de31921a54bffe496048fc
Lorenzo Giuliani <lorenzo@quantfi.tech>
2024-08-05 12:13:27 +0200

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "qmk userspace";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flakelight.url = "github:nix-community/flakelight";
   };
   outputs = {self, flakelight, nixpkgs, ...}@inputs: flakelight ./. {
