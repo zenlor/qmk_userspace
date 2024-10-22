663751d132e86063687152314db0c84910eb8894
Lorenzo Giuliani <lorenzo@quantfi.tech>
2024-10-22 13:30:03 +0200

    falbatech has a different pinout than official atreus

--- keyboards/atreus/keymaps/via/keymap.c
+++ keyboards/atreus/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                       KC_Y,   KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                       KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                       KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC, MO(_RS), KC_MINS, KC_QUOT, KC_ENT  ),
+    KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC,  KC_LALT, KC_LCTL, KC_SPC, MO(_RS), KC_MINS, KC_QUOT, KC_ENT  ),
 
   /*
    *  !       @     up     {    }        ||     pgup    7     8     9    *
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_DEL,  KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
+    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_DEL,  KC_LALT, KC_LCTL, KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL  ),
   /*
    * insert home   up  end   clear cmos ||      up     F7    F8    F9   F10
    *  del   left  down right bootloader ||     down    F4    F5    F6   F11
@@ -40,11 +40,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  EE_CLR,                      KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, QK_BOOT,                     KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     TG(_GM), KC_VOLU, KC_NO,   KC_NO,   QK_REBOOT,                   KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC,   KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLSH, KC_PAUS ),
-
-  [_GM] = LAYOUT( /* GM layer */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_7,     KC_8, KC_9, KC_0, KC_MINUS ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_1,     KC_2, KC_3, KC_4, KC_5     ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                   KC_GRAVE, KC_Q, KC_W, KC_E, KC_R     ,
-    TO(_QW), KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_TAB, KC_F, KC_SPACE, KC_A, KC_S, KC_D, KC_V     )
+    TO(_QW), KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC,   KC_LALT, KC_LCTL, KC_SPC,  TO(_QW), KC_PSCR, KC_SLSH, KC_PAUS ),
 };
